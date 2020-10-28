efe40b3927d083e68eb51812929f4621fb250c9a
csgrabt <gy.cseko@outlook.com>
2020-10-28 19:49:58 +0100

    Nem egyértelmű teljesen a feladat. A Song osztályba előadót kell tárolni, viszont a Music résznél már szerzőt kell megadni. Meglátásom szerint a kettő nem minden esetben egyezik meg. Továbbá a felhozott példában ki kell írni a hosszát a számnak másodpercben (mp) viszont az utána következő sorban már percben van megadva a hossz, ezt úgy oldottam meg, hogy bekértem az adatot mp-be, és a kiírásnál már percben jelenítettem meg az értéket.

---
+++ src/main/java/classstructureattributes/Music.java
@@ -0,0 +1,18 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class Music {
+
+    public static void main(String[] args) {
+        Song song = new Song();
+        System.out.println("Adja meg a kedvenc zeneszáma adatait (szerző - cím - hossz (mp)):");
+        Scanner scanner = new Scanner(System.in);
+        song.band = scanner.nextLine();
+        song.title = scanner.nextLine();
+        song.lenght= scanner.nextInt();
+
+        System.out.println(song.band + " - " + song.title + " (" + song.lenght/60 + " perc )");
+
+    }
+}
