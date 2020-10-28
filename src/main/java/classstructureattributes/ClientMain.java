5857147b15c8d539a1d41e7060fcd855f227451a
csgrabt <gy.cseko@outlook.com>
2020-10-28 19:32:31 +0100

    Objektumok és atribútumok Ügyfél osztály nevű feladata.

---
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,28 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+        System.out.println("Adja meg a nevét, címét, és születési idejét:");
+        Scanner scanner= new Scanner(System.in);
+        client.name = scanner.nextLine();
+        client.address = scanner.nextLine();
+        client.year = scanner.nextInt();
+
+        System.out.println("A megadott adatok: " + client.name  + ", " + client.address + ", " + client.year );
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
