f3290a7ebbf1c53f41becc6bac4019872b3833ce
csgrabt <gy.cseko@outlook.com>
2020-10-29 20:00:58 +0100

    Termék feladat megoldása

---
+++ src/main/java/classsctructureconstructors/Product.java
@@ -0,0 +1,28 @@
+package classsctructureconstructors;
+
+public class Product {
+
+    private String name;
+    private int price;
+
+public Product(String name, int price){
+    this.name = name;
+    this.price = price;
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+public void incrasePrice(int amount){
+    this.price = this.price + amount;}
+
+public void decreasePrice(int amount) {
+this.price = this.price - amount;}
+}
+
+
+
---
+++ src/main/java/classsctructureconstructors/ProductMain.java
@@ -0,0 +1,29 @@
+package classsctructureconstructors;
+
+import java.util.Scanner;
+
+public class ProductMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mi a termék neve?");
+        String name1 = scanner.nextLine();
+        System.out.println("Ára?");
+        int price1 = scanner.nextInt();
+
+
+        Product product = new Product (name1, price1);
+
+
+        System.out.println("A termék neve: " + product.getName() + " A termék ára: " + product.getPrice());
+
+        System.out.println("Mennyive nőtt az ár?");
+        product.incrasePrice(scanner.nextInt());
+        System.out.println(product.getPrice());
+
+        System.out.println("Mennyive csökken az ára?");
+        product.decreasePrice(scanner.nextInt());
+        System.out.println(product.getPrice());
+    }
+
+}
