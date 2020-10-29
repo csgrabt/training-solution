f42ad20c0050713dc067d5bdc082e402a3fee294
csgrabt <gy.cseko@outlook.com>
2020-10-29 13:02:47 +0100

    Ügyfél osztály feladat megoldása

---
+++ src/main/java/classsctructuremethods/ClientMain.java
@@ -0,0 +1,24 @@
+package classsctructuremethods;
+
+import java.util.Scanner;
+
+public class  ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.setName("Bélafalvi Béla");
+        client.setYear(1984);
+        client.setAddress("Bélapátfalva");
+        System.out.println(client.getAddress());
+        System.out.println(client.getYear());
+        System.out.println(client.getName());
+        System.out.println("Adja meg az új címét");
+        Scanner scanner= new Scanner(System.in);
+        String newAddress = scanner.nextLine();
+
+        client.migrate(newAddress);
+
+        System.out.println("Új cím " + client.getAddress());
+
+
+    }
+}
