15821832856f774e3cf25fedb11da1d6cb1bea3e
csgrabt <gy.cseko@outlook.com>
2020-10-29 11:55:53 +0100

    kicsit alakítottam rajta, így ugyanúgy működik mint az előző, de rövidebb lett a kód

--- src/main/java/classstructureio/Registration.java
+++ src/main/java/classstructureio/Registration.java
@@ -8,8 +8,8 @@ public class Registration {
         Scanner scanner= new Scanner(System.in);
         String name = scanner.nextLine();
 
-        Scanner scanner2= new Scanner(System.in);
-        String email = scanner2.nextLine();
+
+        String email = scanner.nextLine();
 
         System.out.println("Neve " + name);
         System.out.println("Email címe: " + email);
