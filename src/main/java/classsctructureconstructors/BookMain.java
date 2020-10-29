0c1b4a6e936e7ddccdffd4a9054f161c932a0eb6
csgrabt <gy.cseko@outlook.com>
2020-10-29 18:45:32 +0100

    Könyv osztály feladat megoldása

---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,29 @@
+package classsctructureconstructors;
+
+import java.util.Scanner;
+
+public class BookMain {
+
+    public static void main(String[] args) {
+        Book book = new Book("Tolkein" , "A király visszatér");
+
+        System.out.println(book.getAuthor());
+        System.out.println(book.getTitle());
+        System.out.println(book.getRegNumber());
+
+
+        System.out.println("Adja meg a regisztációs számot:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        book.register(scanner.nextLine());
+
+        System.out.println(book.getRegNumber());
+
+    }
+
+
+
+
+
+}
