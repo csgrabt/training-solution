1e8ad777fdd7600d12e7961d6f5016c199772473
csgrabt <gy.cseko@outlook.com>
2020-10-29 21:08:19 +0100

    Bank   Ez nehéz volt

---
+++ src/main/java/classsctructureconstructors/Bank.java
@@ -0,0 +1,61 @@
+package classsctructureconstructors;
+
+import java.util.Scanner;
+
+
+public class Bank {
+
+    public static void main(String[] args) {
+
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Adja meg a Bankszámla Számot!");
+        String number1 = scanner.nextLine();
+        System.out.println("Adja meg a nevét!");
+        String name1 = scanner.nextLine();
+        System.out.println("Adja meg az egyenlegét!");
+        int balance1 = scanner.nextInt();
+
+        scanner.nextLine();
+
+
+
+
+    BankAccount acc1 = new BankAccount(number1,name1, balance1);
+
+        System.out.println(acc1.getInfo());
+
+// második ügyfél
+        System.out.println("Adja meg a Bankszámla Számot!");
+        String number2 = scanner.nextLine();
+        System.out.println("Adja meg a nevét!");
+        String name2 = scanner.nextLine();
+        System.out.println("Adja meg az egyenlegét!");
+        int balance2 = scanner.nextInt();
+
+        BankAccount acc2 = new BankAccount(number2, name2, balance2);
+
+        System.out.println(acc1.getInfo());
+        System.out.println(acc2.getInfo());
+
+
+        System.out.println("Mennyi pénzt szeretne kivenni?");
+        acc1.withdraw(scanner.nextInt());
+        System.out.println(acc1.getInfo());
+
+        System.out.println("Mennyi pénzt szeretne betenni?");
+        acc2.deposit(scanner.nextInt());
+        System.out.println(acc2.getInfo());
+
+        System.out.println("Mennyi pénzt utalna?");
+        acc1.transfer(acc2, scanner.nextInt());
+
+
+
+
+        System.out.println(acc1.getInfo());
+        System.out.println(acc2.getInfo());
+
+
+    }
+
+}
---
+++ src/main/java/classsctructureconstructors/BankAccount.java
@@ -0,0 +1,38 @@
+package classsctructureconstructors;
+
+public class BankAccount {
+
+private String accountNumber;
+private String owner;
+private int balance;
+
+public BankAccount(String accountNumber, String owner, int balance){
+    this.accountNumber = accountNumber;
+    this.owner = owner;
+    this.balance = balance;
+}
+public void deposit(int amount){
+    this.balance = this.balance + amount;}
+
+public void withdraw(int amount) {
+    this.deposit(-amount);
+}
+
+    public String getInfo() {
+        return owner + " (" + accountNumber + "): " + balance + " Ft" ;
+    }
+
+ public void transfer(BankAccount to, int amount){
+    this.withdraw(amount);
+    to.deposit(amount);
+}
+
+
+}
+
+
+
+
+
+
+
