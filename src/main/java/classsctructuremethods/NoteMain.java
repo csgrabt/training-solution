4c4aead24a0f8238553f01031bc95e24b1583b64
csgrabt <gy.cseko@outlook.com>
2020-10-29 14:47:13 +0100

    Jegyzeteljünk feladat megoldása

---
+++ src/main/java/classsctructuremethods/NoteMain.java
@@ -0,0 +1,26 @@
+package classsctructuremethods;
+
+public class NoteMain {
+
+    public static void main(String[] args) {
+        Note note = new Note ();
+    note.setName("name");
+        note.setText("text");
+        note.setTopic("topic");
+
+
+        System.out.println(note.getNoteText());
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
