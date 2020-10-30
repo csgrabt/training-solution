c7bb7ae6aee3b528c9cc332e4c0781e8f739b4dc
csgrabt <gy.cseko@outlook.com>
2020-10-30 11:46:44 +0100

    Távolság feladat megoldása

---
+++ src/main/java/localvariables/Distance.java
@@ -0,0 +1,21 @@
+package localvariables;
+
+public class Distance {
+   private double x1;
+   private boolean b;
+
+   public Distance(double x1, boolean b){
+       this.x1 = x1;
+       this.b = b;
+
+   }
+
+
+    public double getX1() {
+        return x1;
+    }
+
+    public boolean isB() {
+        return b;
+    }
+}
---
+++ src/main/java/localvariables/DistanceMain.java
@@ -0,0 +1,12 @@
+package localvariables;
+
+
+public class DistanceMain {
+
+    public static void main(String[] args) {
+        Distance dis1 = new Distance(854754.5475, true);
+        System.out.println("Távolság:" +dis1.getX1() + "helyes a távolság? " + dis1.isB());
+
+    }
+
+}
