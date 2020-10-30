d80fe4ff2c46507078739b63a78f15055c1528d4
csgrabt <gy.cseko@outlook.com>
2020-10-30 11:50:37 +0100

    Javítva

--- src/main/java/localvariables/DistanceMain.java
+++ src/main/java/localvariables/DistanceMain.java
@@ -7,6 +7,9 @@ public class DistanceMain {
         Distance dis1 = new Distance(854754.5475, true);
         System.out.println("Távolság:" +dis1.getX1() + "helyes a távolság? " + dis1.isB());
 
+        int i = (int) dis1.getX1();
+        System.out.println(i);
+
     }
 
 }
