4446fbcc9fb7c4076b9f05e74603469c2a621668
csgrabt <gy.cseko@outlook.com>
2020-10-31 16:20:47 +0100

    String osztály megoldás

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,16 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+
+
+        String string = "Hello world!";
+
+
+        System.out.println("Hello World!".toUpperCase());
+
+
+        System.out.println(string.toUpperCase());
+    }
+}
