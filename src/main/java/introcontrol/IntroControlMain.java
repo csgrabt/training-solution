5fab27db63f39410aa2cb9ce2a69908cd8fc00b4
csgrabt <gy.cseko@outlook.com>
2020-11-01 18:28:50 +0100

    Bevezetés a vezérlési szerkezetek használatába megoldása

---
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,23 @@
+package introcontrol;
+
+public class IntroControlMain {
+
+    public static void main(String[] args) {
+
+        IntroControl introControl = new IntroControl();
+
+        //System.out.println(introControl.substractTenIfGreaterThanTen(100));
+
+        //System.out.println(introControl.describeNumber(0));
+        //System.out.println(introControl.greetingToJoe("Joe"));
+        //System.out.println(introControl.calculateConsumption(1, 0));
+        //introControl.printNumbers(15);
+        //introControl.printNumbersBetween(1, 5);
+
+        //introControl.printNumbersBetweenAnyDirection(10, 10);
+
+
+        introControl.printOddNumbers(51);
+
+    }
+}
