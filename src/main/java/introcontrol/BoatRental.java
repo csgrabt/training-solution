9388e23ca5a15eda0c18931b24e13a21ddbc662e
csgrabt <gy.cseko@outlook.com>
2020-11-04 19:44:28 +0100

    First commit

---
+++ src/main/java/introcontrol/BoatRental.java
@@ -0,0 +1,21 @@
+package introcontrol;
+
+import java.util.Scanner;
+
+public class BoatRental {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adja meg az emberek számát!");
+        int number = scanner.nextInt();
+
+        if (number > 10)
+            System.out.println("Nincs elég hely");
+
+
+
+
+
+    }
+}
