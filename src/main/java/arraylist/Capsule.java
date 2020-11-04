f39d650239770b74e59a9ab8cf4dbf924ad1b42d
csgrabt <gy.cseko@outlook.com>
2020-11-04 17:58:39 +0100

    Capsule feladat megoldása

---
+++ src/main/java/arraylist/Capsule.java
@@ -0,0 +1,81 @@
+package arraylist;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+
+public class Capsule {
+
+    public List<String> szinek = new ArrayList<>();
+
+
+    public void addfirst(String szin) {
+        szinek.add(0, szin);
+    }
+
+
+    public void addLast(String szin) {
+        szinek.add(szin);
+    }
+
+
+    public List<String> getSzinek() {
+        return szinek;
+    }
+
+    public void removeFirst() {
+        szinek.remove(0);
+    }
+
+    public void removeLast() {
+        szinek.remove(szinek.size() - 1);
+    }
+
+
+    public static void main(String[] args) {
+        Capsule capsule = new Capsule();
+        capsule.addfirst("béla");
+
+        System.out.println(capsule.getSzinek());
+
+        capsule.addfirst("béla2");
+
+        System.out.println(capsule.getSzinek());
+
+        capsule.addLast("béla3");
+
+        System.out.println(capsule.getSzinek());
+
+        capsule.szinek.add("sdfds");
+
+        System.out.println(capsule.getSzinek());
+
+        capsule.removeFirst();
+
+        System.out.println(capsule.getSzinek());
+
+        capsule.removeLast();
+
+        System.out.println(capsule.getSzinek());
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
