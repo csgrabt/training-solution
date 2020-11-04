4e8a1766b88327964d0fecf4999adf98708ad416
csgrabt <gy.cseko@outlook.com>
2020-11-04 15:01:44 +0100

    Számológép feladat megoldása

---
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -0,0 +1,30 @@
+package arrayofarrays;
+
+public class ArrayOfArraysMain {
+
+    private int size;
+
+    public int[][] multiplicationTable(int size) {
+        int[][] b = new int[size][size];
+        for (int i = 0; i < size; i++){
+            for( int j=0; j < size; j++){
+                b[i][j] = (i+1)*(j+1);
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return b;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        ArrayOfArraysMain arrayOfArraysMain = new ArrayOfArraysMain();
+
+        arrayOfArraysMain.multiplicationTable(1);
+
+
+    }
+}
+
