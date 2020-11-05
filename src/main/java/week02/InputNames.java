1f2e9264bffad8ae152aea6336da33632d2c6b5f
csgrabt <gy.cseko@outlook.com>
2020-11-05 18:15:01 +0100

    first commit

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,19 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+
+        String[] array = new String[5];
+
+for (int i = 0; i <= array.length; i++);
+        System.out.println("Adja meg a nevet:");
+         ;
+    }
+
+}
+
