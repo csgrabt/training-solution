79ea9d9bc457500a414fce0c1235d2706f75140a
csgrabt <gy.cseko@outlook.com>
2020-11-06 10:15:01 +0100

    Nyelvek feladat

---
+++ src/main/java/week02/Languages.java
@@ -0,0 +1,29 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+    public static void main(String[] args) {
+        List<String> lista  = new ArrayList<>();
+
+
+        lista.add("Java");
+        lista.add("Python");
+        lista.add("JavaScript");
+
+
+        for (String alma : lista) {
+if (alma.length() > 5) {
+    System.out.println(alma);
+}
+        }
+
+
+
+
+    }
+
+
+
+}
