449d5b9968073315ba8751205f6fa22026da6226
csgrabt <gy.cseko@outlook.com>
2020-11-07 19:12:19 +0100

    Referenciák 2. fveladatának megoldása

---
+++ src/main/java/reference/parameters/ReferenceMain.java
@@ -0,0 +1,33 @@
+package reference.parameters;
+
+public class ReferenceMain {
+    public static void main(String[] args) {
+
+
+        /*Person person2;
+
+        Person person3;
+
+        person2 = new Person("Bidden", 85);
+
+        person3 = person2;
+
+        System.out.println(person3.getName());
+
+        person3.setName("Trump");
+
+        System.out.println(person2.getName());
+        System.out.println(person3.getName());*/
+
+        int a = 24;
+
+        int b = a;
+
+        b = b+1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
