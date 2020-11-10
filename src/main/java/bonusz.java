58d9659fbb1c8e2adc76077a41366bad0231b0da
csgrabt <gy.cseko@outlook.com>
2020-11-10 17:48:59 +0100

    idk

---
+++ src/main/java/bonusz.java
@@ -0,0 +1,13 @@
+public enum bonusz {
+
+
+    ALKALMAZOTT(10),
+    IGAZGATO(200),
+    IGAZGATOJELYETTES(400),
+    CEO(800),
+    ;
+private int values;
+    bonusz(int values) {
+        this.values=values;
+    }
+}
---
+++ src/main/java/week3/Position.java
@@ -0,0 +1,20 @@
+package week3;
+
+public class Position {
+
+private String name;
+private Enum bonusz;
+
+
+    public Position(String name, Enum bonusz) {
+        this.name = name;
+        this.bonusz = bonusz;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
