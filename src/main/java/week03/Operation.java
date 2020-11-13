40183da6fd863602d2fd6bfb683171d1fe88078f
csgrabt <gy.cseko@outlook.com>
2020-11-13 16:08:28 +0100

    Órai munka

---
+++ src/main/java/week03/Operation.java
@@ -0,0 +1,21 @@
+package week03;
+
+public class Operation {
+private int left;
+private int right;
+
+    public Operation(String string) {
+        int b = string.indexOf('+');
+        String leftString = string.substring(0, b);
+        String rightString = string.substring(b+1);
+        this.left = Integer.parseInt(leftString);
+        this.right =Integer.parseInt(rightString);
+    }
+public int getResult(){
+        return left+right;
+}
+
+
+}
+
+
---
+++ src/test/java/week03/OperationTest.java
@@ -0,0 +1,17 @@
+package week03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationTest {
+
+    @Test
+void test1() {
+        Operation operation = new Operation("100+250");
+        assertEquals(350, operation.getResult());
+
+
+    }
+
+    }
