c1af574a9f4d55c7cbfaec84dcf41a9a3aed0db6
csgrabt <gy.cseko@outlook.com>
2020-11-14 16:39:01 +0100

    String Buildek Petes Vetes feladat megoldása

---
+++ src/main/java/pets/Pet.java
@@ -0,0 +1,39 @@
+package pets;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+
+    private String name;
+    private LocalDate yearOfBirth;
+    private Gender gender;
+    private String regNumber;
+
+
+    public Pet(String name, LocalDate yearOfBirth, Gender gender, String regNumber) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.gender = gender;
+        this.regNumber = regNumber;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+}
+
