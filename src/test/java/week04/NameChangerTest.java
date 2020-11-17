61465ca1be9a93de063118508122441a2ff51972
csgrabt <gy.cseko@outlook.com>
2020-11-17 18:10:01 +0100

    push

---
+++ src/test/java/week04/NameChangerTest.java
@@ -0,0 +1,21 @@
+package week04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameChangerTest {
+    @Test
+ public void testConstuctor(){
+       IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, ()-> new NameChanger(""));
+assertEquals("FullName is null!", iae.getMessage());
+    }
+@Test
+    public void changeNameTest(){
+        NameChanger nameChanger = new NameChanger( "AAA BBB");
+        nameChanger.changeFirstname("CCC");
+        assertEquals("AAA CCC", nameChanger.getFullName());
+
+}
+
+}
