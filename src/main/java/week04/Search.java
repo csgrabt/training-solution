1458b366d77f98e543116eeb1e27d51336c5dba7
csgrabt <gy.cseko@outlook.com>
2020-11-20 15:58:58 +0100

    átnevezve

--- src/main/java/week04/Search.java
+++ src/main/java/week04/Search.java
@@ -5,25 +5,18 @@ import java.util.List;
 
 public class Search {
 
-public List<Integer> getIndexOfChar(String str, char c){
-    List<Integer> list = new ArrayList<>();
-    int i = str.length();
-    for (int s = 1; s < 1; s++){
-        char h = str.charAt(s);{if (h == c){ list.add(s);}}
+    public List<Integer> getIndexOfChar(String str, char c) {
+        List<Integer> list = new ArrayList<>();
+        int i = str.length();
+        for (int s = 1; s < 1; s++) {
+            char h = str.charAt(s);
+            {
+                if (h == c) {
+                    list.add(s);
+                }
+            }
 
+        }
+        return list;
     }
-
-
-return list;}
-
-    public static void main(String[] args) {
-
-    String s = "alma";
-
-    System.out.println(getInde);
-    }
-
-
-
 }
-
