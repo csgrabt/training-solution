99e8c5abb8f2d203872a266257011d0071502d9e
csgrabt <gy.cseko@outlook.com>
2020-11-20 15:54:12 +0100

    lehet kész

---
+++ src/main/java/week04/SearchTest.java
@@ -0,0 +1,24 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchTest {
+
+public List<Integer> getIndexOfChar(String str, char c){
+    List<Integer> list = new ArrayList<>();
+    int i = str.length();
+    for (int s = 1; s < 1; s++){
+        char h = str.charAt(s);{if (h == c){ list.add(s);}}
+
+    }
+
+
+return list;}
+
+
+
+
+
+}
+
