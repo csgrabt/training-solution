3afa0e5c71185a031792263b28f79363c483fc46
csgrabt <gy.cseko@outlook.com>
2020-11-24 16:01:41 +0100

    formáztam, a későbbiekben folytatom az egyszerűsítést

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -3,24 +3,30 @@ package week05d02;
 public class ChangeLetter {
 
 
+    public String changeVowels(String str) {
+        if (str == null) {
+            throw new IllegalArgumentException("String is null!");
+        }
+        if (str.isEmpty()) {
+            throw new IllegalArgumentException("String is Empty!");
+        }
+        String csereltstr = "";
+        int stringLenght = str.length();
+        str = str.toLowerCase();
+        for (int i = 0; i < stringLenght; i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
+                    || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                csereltstr = csereltstr + '*';
+            } else {
+                csereltstr = csereltstr + str.charAt(i);
+            }
 
-public String changeVowels(String str){
-    if( str == null){ throw new IllegalArgumentException("String is null!");}
-    if( str.isEmpty()){ throw new IllegalArgumentException("String is Empty!");}
-    String csereltstr = "";
-    int stringLenght = str.length();
-    str = str.toLowerCase();
-    for (int i = 0; i < stringLenght; i++){
-        if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
-                || str.charAt(i) == 'o' || str.charAt(i) == 'u'){csereltstr = csereltstr + '*';}else{csereltstr = csereltstr + str.charAt(i);}
+        }
 
+        return str = csereltstr;
     }
 
-    return str = csereltstr;}
 
-
-
-
-    }
+}
 
 
