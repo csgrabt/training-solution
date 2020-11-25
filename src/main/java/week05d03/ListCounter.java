b2a948aea49a28e4bee8a66c01a109797a518e58
csgrabt <gy.cseko@outlook.com>
2020-11-25 15:41:38 +0100

    javítottam

--- src/main/java/week05d03/ListCounter.java
+++ src/main/java/week05d03/ListCounter.java
@@ -5,24 +5,15 @@ import java.util.List;
 
 public class ListCounter {
 
-private List<String> nameList = new ArrayList<>();
 
-    public ListCounter(List<String> nameList) {
-        this.nameList = nameList;
-    }
-
-
-    public List<String> getNameList() {
-        return nameList;
-    }
 
     public static final char searchedCharacter = 'a';
     public static final int indexOfChar = 0;
 
-public int counterAa(){
+public int counterAa(List<String> list){
    int count = 0;
-   for (int i = 0; i < nameList.size(); i++){
-       if (nameList.get(i).toLowerCase().charAt(indexOfChar) == searchedCharacter){count++;}
+   for (int i = 0; i < list.size(); i++){
+       if (list.get(i).toLowerCase().charAt(indexOfChar) == searchedCharacter){count++;}
    }
     return count;}
 
