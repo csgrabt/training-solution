fe05eeb8b04afcb8493127005c6c392c6d340c47
csgrabt <gy.cseko@outlook.com>
2020-11-26 15:48:00 +0100

    First commit, még nincs tesztelve a második bónusz

---
+++ src/main/java/week05d04/Store.java
@@ -0,0 +1,54 @@
+package week05d04;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+
+    private List<Product> productList = new ArrayList<>();
+
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+
+    //bonusz 1
+    public int getNumberOfExpired() {
+        int expired = 0;
+
+        for (int i = 0; i < productList.size(); i++) {
+            if (productList.get(i).getExpirationDate().isBefore(LocalDate.now())) {
+                expired++;
+            }
+
+        }
+
+
+        return expired;
+    }
+
+
+//bónusz2
+
+
+    public boolean isExperied(Product product) {
+        boolean a = false;
+        return product.getExpirationDate().isAfter(LocalDate.now());
+    }
+
+    public boolean listNotHave(Product product) {
+boolean b = false;
+if (isExperied(product)){
+    for (Product p : productList
+         ) {p.getName().equals(product.getName());
+        b = productList.add(product);
+    }
+}
+    return b;}
+}
