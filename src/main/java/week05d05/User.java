97c501e1df6410a6eae84ea2c2b5165383ebc27b
csgrabt <gy.cseko@outlook.com>
2020-11-27 15:40:48 +0100

    Legyeszerűbb megoldás még bónuszok nélkül

---
+++ src/main/java/week05d05/User.java
@@ -0,0 +1,36 @@
+package week05d05;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(String firstName, String lastName, String email) {
+        if (email.indexOf("@") ==  -1){throw new IllegalArgumentException("Invalid format, no @");}
+        if (email.indexOf(".") ==  -1){throw new IllegalArgumentException("Invalid format, no .");}
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName(){
+        String fullName = firstName +  " " + lastName;
+
+    return fullName;
+    }
+
+
+}
