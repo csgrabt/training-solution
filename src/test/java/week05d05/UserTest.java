bd5263693b28fc28f7ed4b0b812acc33f0da783d
csgrabt <gy.cseko@outlook.com>
2020-11-27 16:42:07 +0100

    mukodik a teszteset is

---
+++ src/test/java/week05d05/UserTest.java
@@ -0,0 +1,47 @@
+package week05d05;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import week05d04.Product;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void productTest() {
+        User user = new User("Liszt", "Béla", "alma@alma.hu");
+
+
+        assertEquals("Liszt", user.getFirstName());
+        assertEquals("Béla", user.getLastName());
+        assertEquals("Liszt Béla", user.getFullName());}
+
+
+
+    @Test
+
+
+
+       public void productTestEmailat() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+               new User("Liszt", "Béla", "almaalma.hu");
+           });
+           assertEquals("no @", ex.getMessage());
+    }
+
+    @Test
+
+    public void productTestEmaildot() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new User("Liszt", "Béla", "alma@almahu");
+        });
+        assertEquals("no .", ex.getMessage());
+    }
+
+
+
+
+}
+
