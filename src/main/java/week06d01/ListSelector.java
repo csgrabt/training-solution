c19a74f9d5a90fa6af2bcf7a2eef4b1a1b30d131
csgrabt <gy.cseko@outlook.com>
2020-11-30 15:42:17 +0100

    vessző nélküi verzió

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,39 @@
+package week06d01;
+
+import week05d03.ListCounter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class ListSelector {
+
+
+
+    public String listSelector(List<String> list){
+        if (list == null){throw new IllegalArgumentException("List is null!");}
+        if (list.isEmpty()){return "";}
+        StringBuilder sb = new StringBuilder("[");
+        for(int i = 0; i < list.size(); i=i+2){
+        sb.append(list.get(i));}
+        sb.append("]");
+        return sb.toString();}
+
+/*    public static void main(String[] args) {
+        ListSelector listSelector = new ListSelector();
+
+        List<String> list= new ArrayList<>();
+list = null;
+        //list.add("alma");
+        //list.add("körte");
+        //list.add("retek");
+        //list.add("TDK");
+        //list.add("hónaalj");
+
+
+        System.out.println(listSelector.listSelector(list));
+
+
+    }*/
+}
