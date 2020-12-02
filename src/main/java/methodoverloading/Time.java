bbae35a65484cb89ced106369ea5e3f44e871467
csgrabt <gy.cseko@outlook.com>
2020-12-02 18:36:36 +0100

    Metódustúlterhelés, Time osztály

---
+++ src/main/java/methodoverloading/Time.java
@@ -0,0 +1,84 @@
+package methodoverloading;
+
+public class Time {
+
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+
+    public Time(int hours) {
+        this.hours = hours;
+    }
+
+
+    public Time(Time time){
+        this.hours = time.getHours();
+        this.minutes = time.getMinutes();
+        this.seconds = time.getSeconds();
+    }
+
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    private int getInSeconds(){
+        return hours*3600+minutes*60+seconds;
+    }
+
+
+    public boolean isEqual(Time time){
+
+    return getInSeconds() == time.getInSeconds();
+    }
+
+
+
+    public boolean isEqual(int hours, int minutes, int seconds){
+    Time time = new Time(hours, minutes, seconds);
+    return getInSeconds() == (time.getInSeconds());}
+
+
+
+
+    public boolean isEarlier(Time time){
+
+        return getInSeconds() < (time.getInSeconds());
+
+    }
+
+
+
+
+
+
+
+    public boolean isEarlier(int hours, int minutes, int seconds){
+
+
+        Time time = new Time(hours, minutes, seconds);
+        return getInSeconds() < (time.getInSeconds());}
+
+    }
+
+
