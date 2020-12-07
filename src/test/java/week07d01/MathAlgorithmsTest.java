94a5c9dc941811e37407947e1012b982139c3339
csgrabt <gy.cseko@outlook.com>
2020-12-07 15:44:07 +0100

    Órai feladat megoldása, nem optimális

---
+++ src/test/java/week07d01/MathAlgorithmsTest.java
@@ -0,0 +1,45 @@
+package week07d01;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import week05d05.User;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static week07d01.MathAlgorithms.isPrime;
+import static week07d01.MathAlgorithms.knownPrim;
+
+public class MathAlgorithmsTest {
+
+
+    @Test
+
+public void primTest1() {
+    assertEquals(true, isPrime(5));
+    assertEquals(false, isPrime(6));
+    assertEquals(false, isPrime(102));
+    assertEquals(true, isPrime(9973));
+
+
+
+}@Test
+    public void primList(){
+        assertEquals(true, isPrime(9973)); // a lista max eleménél nagyobb prím
+        assertEquals(5, knownPrim.size()); }// a lista max eleménél nagyobb prím
+
+
+
+
+@Test
+
+    public void xIsbelow1() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            isPrime(1);
+        });
+        assertEquals("Invalid number!", ex.getMessage());
+    }
+
+
+
+
+
+}
