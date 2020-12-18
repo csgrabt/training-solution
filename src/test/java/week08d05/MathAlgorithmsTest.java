66342702586b08b3eb6541e4ecdfaf05fb1a2fb4
csgrabt <gy.cseko@outlook.com>
2020-12-18 15:37:06 +0100

    week08d05 feladat megoldása + teszt

---
+++ src/test/java/week08d05/MathAlgorithmsTest.java
@@ -0,0 +1,39 @@
+package week08d05;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import week08d02.Lottery;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static week07d01.MathAlgorithms.isPrime;
+
+public class MathAlgorithmsTest {
+
+
+
+
+    @Test
+    public void mathAlgoritms() {
+
+        assertEquals(2, MathAlgorithms.greatestCommonDivisor(8, 6));
+        assertEquals(2, MathAlgorithms.greatestCommonDivisor(6, 4));
+        assertEquals(2, MathAlgorithms.greatestCommonDivisor(-4, 6));
+        assertEquals(2, MathAlgorithms.greatestCommonDivisor(-6, -4));
+        assertEquals(1, MathAlgorithms.greatestCommonDivisor(-21, -2));
+    }
+
+
+
+    @Test
+
+    public void isNegative() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            MathAlgorithms.greatestCommonDivisor(-5, 0);
+        });
+        assertEquals("Cannot be divided by zero", ex.getMessage());
+    }
+
+
+
+
+}
