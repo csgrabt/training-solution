e0a38ccb6dd16dd3a954c894fb10c09352a1a419
csgrabt <gy.cseko@outlook.com>
2021-01-12 17:55:14 +0100

    csoportmunkav.000000001

--- src/main/java/week11d02/Courier.java
+++ src/main/java/week11d02/Courier.java
@@ -20,29 +20,55 @@ public class Courier {
         if (ride == null) {
             throw new IllegalArgumentException("Ride is null");
         }
-        if (rides.size() == 0){rides.add(ride);}
-        if (rides.get(rides.size() - 1).getDay() <= ride.getDay() &&
-                rides.get(rides.size() - 1).getCounter() < ride.getCounter()) {
-            rides.add(ride);
+        if (rides.isEmpty()) {
+            if (ride.getCounter() == 1) {
+                rides.add(ride);
+                return;
+            } else {
+                throw new IllegalArgumentException("Nem a nap első köre");
+            }
         }
+        int lastDay = rides.get(rides.size() - 1).getDay();
+        int lastCounter = rides.get(rides.size() - 1).getDay();
+
+        int newDay = ride.getDay();
+        int newCounter = ride.getCounter();
+
+        if (lastDay < newDay && newCounter == 1) {
+            rides.add(ride);
+            return;
+        }
+        if (lastDay == newDay && lastCounter + 1 == newCounter) {
+            rides.add(ride);
+            return;
+        }
+
+        throw new IllegalArgumentException("A ride példány nem illeszthető a sorba!");
 
     }
 
-    public int freeDay() {
-        int result = -1;
 
-        for (int i = 1; i < rides.get(rides.size()).getDay(); i++) {
+    public int freeDay() {
+        int counter = 0;
+
+        for (int i = 1; i < 8; i++) {
             for (Ride item : rides
             ) {
-                if (i != item.getDay()) {
-                    result = i;
+                if (i == item.getDay()) {
+                    counter++;
+
+
                 }
 
             }
+            if (counter == 0) {
+                return i;
+            }
+            counter = 0;
         }
 
 
-        return result;
+        return -1;
     }
 
 
--- src/test/java/week11d02/CourierTest.java
+++ src/test/java/week11d02/CourierTest.java
@@ -2,6 +2,7 @@ package week11d02;
 
 import org.junit.jupiter.api.Test;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,43 +11,87 @@ import static org.junit.jupiter.api.Assertions.*;
 class CourierTest {
 
     @Test
-    void addRide() {
-        List<Ride> rides = new ArrayList<>();
-        rides.add(new Ride(1, 1, 1));
-        rides.add(new Ride(1, 2, 1));
-        rides.add(new Ride(3, 1, 1));
+    void addRideIsNull() {
+        Courier courier = new Courier(List.of(new Ride(1, 1, 1)));
+        Exception actualException = assertThrows(IllegalArgumentException.class, () -> {
+            courier.addRide(null);
+        });
+        assertEquals("Ride is null", actualException.getMessage());
 
-    Courier courier = new Courier(rides);
+    }
 
-    assertEquals(3, courier.getRides().size());
-
-
+    @Test
+    void addFirstRide() {
+        List<Ride> list = new ArrayList<>();
+        Courier courier = new Courier(list);
+        Ride ride = new Ride(1, 1, 1);
+        courier.addRide(ride);
+        assertEquals(1, courier.getRides().size());
     }
 
 
     @Test
-    void addRideError() {
-        List<Ride> rides = new ArrayList<>();
-        Courier courier = new Courier(rides);
-
-        courier.addRide(new Ride(1, 2, 1));
-        courier.addRide(new Ride(1, 1, 1));
-        courier.addRide(new Ride(3, 1, 1));
-
-        assertEquals(1, courier.getRides().size());
-
-
-
-
-
-
-
-
+    void addFirstRideError() {
+        List<Ride> list = new ArrayList<>();
+        Courier courier = new Courier(list);
+        Ride ride = new Ride(1, 2, 1);
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            courier.addRide(ride);
+        });
+        assertEquals("Nem a nap első köre", ex.getMessage());
     }
-
 
 
     @Test
     void freeDay() {
+        Courier courier = new Courier(List.of(new Ride(1, 1, 1),
+                new Ride(1, 2, 3),
+                new Ride(3, 1, 1)
+        ));
+
+
+        assertEquals(2, courier.freeDay());
+
+
     }
+
+    @Test
+    void addRideDayIsDifferent() {
+        List<Ride> list = new ArrayList<>();
+        Courier courier = new Courier(list);
+        Ride ride = new Ride(1, 1, 1);
+        courier.addRide(ride);
+        Ride ride2 = new Ride(2, 1, 1);
+        courier.addRide(ride2);
+        assertEquals(2, courier.getRides().size());
+    }
+
+    @Test
+    void addRideDayIsSame() {
+        List<Ride> list = new ArrayList<>();
+        Courier courier = new Courier(list);
+        Ride ride = new Ride(1, 1, 1);
+        courier.addRide(ride);
+        Ride ride2 = new Ride(1, 2, 1);
+        courier.addRide(ride2);
+        assertEquals(2, courier.getRides().size());
+    }
+
+
+    @Test
+    void addRideDayInNotOrder() {
+        List<Ride> list = new ArrayList<>();
+        Courier courier = new Courier(list);
+        Ride ride = new Ride(1, 1, 1);
+        courier.addRide(ride);
+        Ride ride2 = new Ride(3, 2, 1);
+
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+        courier.addRide(ride2);
+        });
+    assertEquals("A ride példány nem illeszthető a sorba!", ex.getMessage());
+
+    }
+
+
 }
