41106c76bf003ae63bba2bda1cc5c4c9d47c2854
csgrabt <gy.cseko@outlook.com>
2021-01-14 16:01:25 +0100

    Órai feladat megoldása

---
+++ src/main/java/week11d04/FilesSum.java
@@ -0,0 +1,57 @@
+/*Adott az aktuális könyvtárban több, fájl, melynek a nevei: number00.txt, number03.txt, number20.txt, egészen 99-ig.
+Nem minden fájl létezik, lehet, hogy bizonyos számhoz tartozó fájl kimaradt. Olvasd be az állományból a benne lévő
+számot, és ezt összegezd! Használd a Files.isRegularFile() metódust arra, hogy megnézd, létezik-e a fájl!
+Használd a Files.readString() metódust a fájl beolvasására! A FilesSum osztály sumNumbers(InputStream) metódusába dolgozz!*/
+
+
+package week11d04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilesSum {
+
+
+    private boolean isValid(String string) {
+
+        return Files.isRegularFile(Path.of(string));
+
+    }
+
+
+    private List<String> makeAList() {
+        List<String> fileNames = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            String filename = "number" + i;
+            if (isValid(filename)) {
+                fileNames.add(filename);
+
+            }
+        }
+        return fileNames;
+    }
+
+
+    private int sumNumbers() {
+
+        List<String> validFiles = makeAList();
+        int counter = 0;
+
+        for (int i = 0; i < validFiles.size(); i++)
+
+            try {
+                counter += Integer.parseInt(Files.readString(Path.of(validFiles.get(i))));
+            } catch (IOException e) {
+                throw new IllegalStateException("Cannot read file!", e);
+            }
+
+
+        return counter;
+    }
+
+
+}
