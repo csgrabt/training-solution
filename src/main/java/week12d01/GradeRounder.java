55905321e6b5abfe0ea91ed47ce79b98e9fbb79d
csgrabt <gy.cseko@outlook.com>
2021-01-18 16:04:43 +0100

    órai feladat, én nem értettem teljesen a feladat kiírást

---
+++ src/main/java/week12d01/GradeRounder.java
@@ -0,0 +1,19 @@
+package week12d01;
+
+public class GradeRounder {
+
+
+    public int[] roundGrades(int[] grades) {
+
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] > 40 && grades[i] % 5 >= 3) {
+                grades[i] = grades[i] / 5 * 5 + 5;
+            }
+        }
+        return grades;
+
+
+    }
+
+
+}
