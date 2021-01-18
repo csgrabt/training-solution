b6790031d124bfdeae032294a09c710717951e06
csgrabt <gy.cseko@outlook.com>
2021-01-18 16:06:17 +0100

    órai feladat, én nem értettem teljesen a feladat kiírást

--- src/main/java/week12d01/GradeRounder.java
+++ src/main/java/week12d01/GradeRounder.java
@@ -6,6 +6,7 @@ public class GradeRounder {
     public int[] roundGrades(int[] grades) {
 
         for (int i = 0; i < grades.length; i++) {
+           if (i >100){throw new IllegalArgumentException("The number is higher than 100!");}
             if (grades[i] > 40 && grades[i] % 5 >= 3) {
                 grades[i] = grades[i] / 5 * 5 + 5;
             }
