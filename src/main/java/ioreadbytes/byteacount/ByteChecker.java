3e54a5cfcf3305bc765421d7cc05e3ac1853c814
csgrabt <gy.cseko@outlook.com>
2021-01-21 19:55:09 +0100

    Optimalizálva

--- src/main/java/ioreadbytes/byteacount/ByteChecker.java
+++ src/main/java/ioreadbytes/byteacount/ByteChecker.java
@@ -1,11 +1,10 @@
 package ioreadbytes.byteacount;
 
-import javax.imageio.stream.ImageInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+
 
 public class ByteChecker {
     public int readBytesAndFindAs(String s) {
@@ -16,16 +15,9 @@ public class ByteChecker {
         try (InputStream ip = Files.newInputStream(path)) {
 
             byte[] buffer = new byte[1000];
-            int size = 0;
+            int size;
             while ((size = ip.read(buffer)) > 0) {
-
-                counter += arrayCounter(buffer);
-
-for (int i = 0; i < buffer.length; i++ ){
-    buffer[i] = 0;
-
-}
-
+                counter += arrayCounter(buffer, size);
             }
 
         } catch (IOException ie) {
@@ -37,20 +29,19 @@ for (int i = 0; i < buffer.length; i++ ){
     }
 
 
-private int arrayCounter(byte[] tomb){
-//65, 97
+    private int arrayCounter(byte[] tomb, int i) {
+        int counter = 0;
 
-   int counter = 0;
-    for (byte n : tomb
-         ) {
-        if (n == 97 ){
-            counter++;
+        for (int g = 0; g < i; g++) {
+            if (tomb[g] == 97) {
+                counter++;
+            }
+
         }
-        
+
+
+        return counter;
     }
-return counter;}
-
-
 
 
 }
