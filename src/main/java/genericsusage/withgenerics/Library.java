e5b063e78e734d646a158716b857719bbda5abda
csgrabt <gy.cseko@outlook.com>
2021-01-27 18:06:56 +0100

    Generikusok feladat megoldása

---
+++ src/main/java/genericsusage/withgenerics/Library.java
@@ -0,0 +1,24 @@
+package genericsusage.withgenerics;
+
+import genericsusage.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    
+    private List<Book> books = new ArrayList<>();
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+
+    public Book getFirstBook(List<Book> books) {
+      if (books == null){throw new NullPointerException("");}
+      if (books.isEmpty()){throw new IllegalArgumentException("Argument should not be empty!");}
+
+
+        return books.get(0);
+    }
+}
