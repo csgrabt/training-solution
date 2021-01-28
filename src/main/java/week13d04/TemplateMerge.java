729d3e6d2506ad55ad32df533f74ba0c021d82f3
csgrabt <gy.cseko@outlook.com>
2021-01-28 15:59:34 +0100

    1. verzió

---
+++ src/main/java/week13d04/TemplateMerge.java
@@ -0,0 +1,59 @@
+package week13d04;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TemplateMerge {
+
+
+    public String merge(Path file, List<Employee> employees) {
+        StringBuilder sb = new StringBuilder();
+
+
+        try (BufferedReader bf = Files.newBufferedReader(file)) {
+
+            String template = bf.readLine();
+
+            for (int i = 0; i < employees.size(); i++) {
+                String template1 = template.replace("{nev}", employees.get(i).getName());
+                sb.append(template1.replace("{ev}", employees.get(i).getYearOfBirth()));
+                if (i < employees.size() - 1) {
+                    sb.append("\n");
+                }
+            }
+
+
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Something went wrong", ioe);
+        }
+
+
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+        TemplateMerge templateMerge = new TemplateMerge();
+
+        Path file = Path.of("temaplate.txt");
+        List<Employee> list = new ArrayList<>();
+
+        list.add(new  Employee( "Kiss Béla", 1984));
+        list.add(new  Employee( "Harcsa Lajos", 1984));
+        list.add(new  Employee( "Csuka Márton", 1984));
+
+
+        System.out.println(templateMerge.merge(file, list));
+
+
+    }
+
+
+}
