bd82a9dcd60448237188b08422b0319eeb5bc962
csgrabt <gy.cseko@outlook.com>
2021-01-31 17:45:59 +0100

    Operatorok elmaradt feladata

---
+++ src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class OperatorsMain {
+    public static void main(String[] args) {
+        System.out.println(new Operators().isEven(2));
+        System.out.println(new Operators().isEven(3));
+        System.out.println(new Operators().isEven(5));
+    }
+
+
+}
