d8a27d0a137c4e23509f61356316083d33355b7f
csgrabt <gy.cseko@outlook.com>
2021-01-31 17:02:04 +0100

    literals elmaradt feladata

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,33 @@
+package literals;
+
+public class LiteralsMain {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("1" + 2);
+        System.out.println("" + 1 + 2);
+
+
+        int number1 = 4;
+        int number2 = 3;
+
+        double quotient = (double) number1 / number2;
+
+
+        System.out.println(quotient);
+
+        String s = "árvíztűrőtükörfúrógép";
+
+        String word = "title".toUpperCase();
+
+
+
+
+        Integer integer = 50;
+        System.out.println(
+                Integer.toBinaryString(10));
+
+
+    }
+}
