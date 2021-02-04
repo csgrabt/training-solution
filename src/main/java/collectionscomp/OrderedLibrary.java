8cb15218bcb9428ae55e7f0f33ec8f50edfc2782
csgrabt <gy.cseko@outlook.com>
2021-02-04 17:18:53 +0100

    Comparator

---
+++ src/main/java/collectionscomp/OrderedLibrary.java
@@ -0,0 +1,51 @@
+package collectionscomp;
+
+import java.text.Collator;
+import java.util.*;
+
+public class OrderedLibrary {
+
+    private List<Book> bookList;
+
+    public OrderedLibrary(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+
+    public List<Book> orderedByTitle() {
+        Collections.sort(bookList, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getTitle().compareTo(o2.getTitle());
+            }
+        });
+        return bookList;
+    }
+
+    public List<Book> orderedByAuthor() {
+
+        Collections.sort(bookList, new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getAuthor().compareTo(o2.getAuthor());
+            }
+        });
+
+
+        return bookList;
+    }
+
+    public List<String> orderedByTitleLocale(Locale locale) {
+        List<String> titleList = new ArrayList<>();
+
+        for (Book item:bookList
+             ) {titleList.add(item.getTitle());
+
+        }
+
+        Collections.sort(titleList, Collator.getInstance(locale));
+
+
+        return titleList;
+    }
+}
