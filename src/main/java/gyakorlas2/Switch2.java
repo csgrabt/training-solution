4efcb2a154f5d9cf7d63223974e9a18bd3240c69
csgrabt <gy.cseko@outlook.com>
2021-02-09 12:59:17 +0100

    gyakorlás

---
+++ src/main/java/gyakorlas2/Switch2.java
@@ -0,0 +1,17 @@
+package gyakorlas2;
+
+import java.util.Arrays;
+
+public class Switch2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Double.MAX_VALUE);
+
+
+
+    }
+
+}
+
+
