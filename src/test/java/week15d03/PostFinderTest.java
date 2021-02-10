6ec479e2590454b650aef0d69e87ea4bee24f680
csgrabt <gy.cseko@outlook.com>
2021-02-10 13:10:03 +0100

    teszt

---
+++ src/test/java/week15d03/PostFinderTest.java
@@ -0,0 +1,86 @@
+package week15d03;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.time.LocalDate;
+
+import java.util.ArrayList;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostFinderTest {
+
+    PostFinder ps = new PostFinder(new ArrayList<>());
+
+    LocalDate date = LocalDate.of(2020, 1, 20);
+
+    Post post = new Post("Budapest, Training360", date, "mellények", "Training360");
+
+
+
+
+    @BeforeEach
+    void beforeEach() {
+        ps.addToList(post);
+
+
+    }
+
+    @Test
+    void testAdd() {
+        ps.addToList(new Post("Budapest, Training360", LocalDate.of(2020, 10, 11), "mellények", "Training360"));
+
+
+        assertEquals(2, ps.getPosts().size());
+
+
+    }
+
+    @Test
+    void testOwnerIsSame() {
+        ps.addToList(new Post("Budapest, Training360", LocalDate.of(2020, 10, 11), "mellények", "Training360"));
+
+        assertEquals(2, ps.findPostsFor("Training360").size());
+    }
+
+    @Test
+    void testOwnerIsDifferent() {
+        ps.addToList(new Post("Budapest, Training360", LocalDate.of(2020, 10, 11), "mellények", "Training36"));
+
+        assertEquals(1, ps.findPostsFor("Training360").size());
+        assertEquals(2, ps.getPosts().size());
+    }
+
+
+    @Test
+    void testDateIsLater() {
+        ps.addToList(new Post("Budapest, Training360", LocalDate.of(2025, 10, 11), "mellények", "Training36"));
+
+        assertEquals(1, ps.findPostsFor("Training360").size());
+        assertEquals(2, ps.getPosts().size());
+    }
+
+
+    @Test
+    void testTitleIsEmpty() {
+        ps.addToList(new Post("", LocalDate.of(2025, 10, 11), "mellények", "Training360"));
+
+        assertEquals(1, ps.findPostsFor("Training360").size());
+        assertEquals(2, ps.getPosts().size());
+    }
+
+    @Test
+    void testContentIsEmpty() {
+        ps.addToList(new Post("Budapest, Training360", LocalDate.of(2025, 10, 11), "mellények", ""));
+
+        assertEquals(1, ps.findPostsFor("Training360").size());
+        assertEquals(2, ps.getPosts().size());
+    }
+
+
+}
