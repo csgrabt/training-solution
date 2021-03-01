19ecbe7306c3cd95b7a2bac42e6f319e30c38452
csgrabt <gy.cseko@outlook.com>
2021-03-01 15:47:33 +0100

    kis módosítások, try catch szerkezetek és do while szerkezetbe tettem az egészet

--- src/main/java/closingproject/SecondMenuPoint.java
+++ src/main/java/closingproject/SecondMenuPoint.java
@@ -8,19 +8,36 @@ import java.util.Scanner;
 
 public class SecondMenuPoint {
 
-    public static void secondMenuPoint(Citizen cz, MariaDbDataSource dataSource, CitizenDao cd, Scanner scanner, int number) {
-        if (number == 2) {
-            System.out.println("Adja meg a fájl elérési útvonalát(pl: C:/alma/alma.txt)!");
-            String path = scanner.nextLine();
 
-            if (!(Files.exists(Path.of(path)))) {
-                throw new IllegalArgumentException("The file is not exist!");
+    public static void secondMenuPoint(Citizen cz, MariaDbDataSource dataSource, CitizenDao cd, Scanner scanner, int number) {
+        String inprogress = "1";
+        do {
+            if (number == 2) {
+                System.out.println("Adja meg a fájl elérési útvonalát(pl: C:/alma/alma.txt)!");
+                String path = scanner.nextLine();
+
+                try {
+                    if (!(Files.exists(Path.of(path)))) {
+                        throw new IllegalArgumentException("A fájl nem létezik!");
+                    }
+
+                    System.out.println("Adja meg az elválasztó karaktert a fájlon belül!");
+                    String regex = scanner.nextLine();
+
+                    cd.writeRegisterFromFileToDb(dataSource, path, regex);
+                } catch (IllegalArgumentException ioe) {
+                    System.out.println(" A művelet egy hiba miatt megszakadt!" + ioe);
+                }
             }
 
-            System.out.println("Adja meg az elválasztó karaktert a fájlon belül!");
-            String regex = scanner.nextLine();
-            cd.writeRegisterFromFileToDb(dataSource, path, regex);
-        }
+            System.out.println("Mit kíván tenni? \n Üssön 1-est majd entert további fájlbeolvasáshoz! \n Minden más karakter bevitelével kilép a programból!");
 
+            inprogress = scanner.nextLine();
+
+
+
+        } while (inprogress.equals('1'));
     }
+
+
 }
