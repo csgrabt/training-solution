fbc76c96744465767fc6e32abecc7af0be95a3b8
csgrabt <gy.cseko@outlook.com>
2021-03-21 13:19:35 +0100

    Refactor

--- src/test/java/week06d01/ListSelectorTest.java
+++ src/test/java/week06d01/ListSelectorTest.java
@@ -2,48 +2,37 @@ package week06d01;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import week05d05.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ListSelectorTest {
+class ListSelectorTest {
+    ListSelector listSelector = new ListSelector();
+    List<String> list = new ArrayList<>();
 
     @Test
-
-    public void listIsNull() {
-        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            ListSelector lS = new ListSelector();
-            List<String> list = new ArrayList<>();
-            list = null;
-            lS.listSelector(list);});
+    void listIsNull() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> listSelector.listSelector(null));
         assertEquals("List is null!", ex.getMessage());
     }
 
     @Test
-    public void listIsEmpty() {
-
-            ListSelector lS = new ListSelector();
-            List<String> list = new ArrayList<>();
-            assertEquals("", lS.listSelector(list));
+    void listIsEmpty() {
+        assertEquals("", listSelector.listSelector(list));
     }
 
 
     @Test
-    public void listIsnotEmpty() {
-
-        ListSelector lS = new ListSelector();
-        List<String> list = new ArrayList<>();
+    void listIsNotEmpty() {
         list.add("kálium");
         list.add("hidrogén");
         list.add("nitrogén");
         list.add("glicerin");
         list.add("alma");
 
-
-        assertEquals("[kálium,nitrogén,alma]", lS.listSelector(list));
+        assertEquals("[kálium,nitrogén,alma]", listSelector.listSelector(list));
     }
 
 }
