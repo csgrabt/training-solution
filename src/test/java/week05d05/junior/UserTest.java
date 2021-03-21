fda42a9979e1a80918da5d021716d4eb67decf2d
csgrabt <gy.cseko@outlook.com>
2021-03-21 15:08:00 +0100

    Refactor

--- src/test/java/week05d05/junior/UserTest.java
+++ src/test/java/week05d05/junior/UserTest.java
@@ -2,44 +2,35 @@ package week05d05.junior;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import week05d05.junior.User;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UserTest {
+class UserTest {
 
     @Test
-    public void productTest() {
+    void productTest() {
         User user = new User("Liszt", "Béla", "alma@alma.hu");
-
-
         assertEquals("Liszt", user.getFirstName());
         assertEquals("Béla", user.getLastName());
-        assertEquals("Liszt Béla", user.getFullName());}
-
+        assertEquals("Liszt Béla", user.getFullName());
+    }
 
 
     @Test
-
-
-
-       public void emailNotContainsAt() {
-        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-               new User("Liszt", "Béla", "almaalma.hu");
-           });
-           assertEquals("Email address does not contain \"@\"", ex.getMessage());
+    void emailNotContainsAt() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new User("Liszt", "Béla", "almaalma.hu")
+        );
+        assertEquals("Email address does not contain \"@\"", ex.getMessage());
     }
 
     @Test
-
-    public void emailNotContainsDot() {
-        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new User("Liszt", "Béla", "alma@almahu");
-        });
+    void emailNotContainsDot() {
+        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new User("Liszt", "Béla", "alma@almahu")
+        );
         assertEquals("Email address does not contain \".\"", ex.getMessage());
     }
-
-
 
 
 }
