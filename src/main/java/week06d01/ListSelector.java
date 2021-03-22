146f73085ebb74003a3848fb73f67e8af07b92e7
csgrabt <gy.cseko@outlook.com>
2021-03-22 18:55:23 +0100

    refactor

--- src/main/java/week06d01/ListSelector.java
+++ src/main/java/week06d01/ListSelector.java
@@ -3,35 +3,30 @@ package week06d01;
 import java.util.List;
 
 
-
 public class ListSelector {
 
 
+    public String listSelector(List<String> list) {
+        validator(list);
 
-    public String listSelector(List<String> list){
-        if (list == null){throw new IllegalArgumentException("List is null!");}
-        if (list.isEmpty()){return "";}
         StringBuilder sb = new StringBuilder("[");
-        for(int i = 0; i < list.size(); i=i+2){
-            sb.append(list.get(i) + ","); }
-        sb.deleteCharAt(sb.length()-1);
+        for (int i = 0; i < list.size(); i = i + 2) {
+            sb.append(list.get(i) + ",");
+        }
+        deleteLastCharacter(sb);
+
         sb.append("]");
-        return sb.toString();}
 
-/*    public static void main(String[] args) {
-        ListSelector listSelector = new ListSelector();
+        return sb.toString();
+    }
 
-        List<String> list= new ArrayList<>();
-list = null;
-        //list.add("alma");
-        //list.add("körte");
-        //list.add("retek");
-        //list.add("TDK");
-        //list.add("hónaalj");
+    private StringBuilder deleteLastCharacter(StringBuilder sb) {
+        return sb.deleteCharAt(sb.length() - 1);
+    }
 
-
-        System.out.println(listSelector.listSelector(list));
-
-
-    }*/
+    private void validator(List<String> list) {
+        if (list == null) {
+            throw new IllegalArgumentException("List is null!");
+        }
+    }
 }
