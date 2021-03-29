1ad4fa623b48be04472740bb37dca1432fa1c340
csgrabt <gy.cseko@outlook.com>
2021-03-29 20:02:53 +0200

    felesleges láthatósági szavak törlése

--- src/test/java/algorithmsmax/sales/SalespersonTest.java
+++ src/test/java/algorithmsmax/sales/SalespersonTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SalespersonTest {
+class SalespersonTest {
 
     @Test
-    public void testCreateIsOk() {
+    void testCreateIsOk() {
         Salesperson person = new Salesperson("Somebody", 1000, 200);
 
         assertEquals(1000, person.getAmount());
