61c21a017c3c4465b9f931bfff7498d12e21f594
csgrabt <gy.cseko@outlook.com>
2021-04-02 16:57:29 +0200

    senior csoport feladata, tesztesetek még hátra vannak

---
+++ src/main/java/week07d04/senior/ShoppingList.java
@@ -0,0 +1,40 @@
+package week07d04.senior;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ShoppingList {
+
+
+    public long calculateSum(String path) {
+        Path path1 = Path.of(path);
+        long result = 0L;
+        try (BufferedReader bf = Files.newBufferedReader(path1)) {
+            String line;
+
+            while ((line = bf.readLine()) != null) {
+                String[] array = line.split(";");
+
+                result += (long)  Integer.parseInt(array[1]) * Integer.parseInt(array[2]);
+
+            }
+
+
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Something went wrong", ioe);
+        }
+
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        ShoppingList sp = new ShoppingList();
+
+        System.out.println(sp.calculateSum("shoppingList"));
+    }
+}
