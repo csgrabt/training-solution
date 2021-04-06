304958df7b6c2cf9d919994fcbc1b47a8cb7258d
csgrabt <gy.cseko@outlook.com>
2021-04-06 16:55:47 +0200

    Refactor

--- src/main/java/week08d02/Lottery.java
+++ src/main/java/week08d02/Lottery.java
@@ -1,40 +1,31 @@
 package week08d02;
 
+
 import java.util.*;
 
 public class Lottery {
 
+    private Lottery() {
+    }
+
+
+
     public static int randomNumber(int interval) {
         Random rnd = new Random();
-        int randomNumber = rnd.nextInt(interval);
-        return randomNumber;
+        return rnd.nextInt(interval);
     }
 
-   public static List<Integer> getNumbers(int interval, int numberOfInteger) {
+    public static List<Integer> getNumbers(int interval, int numberOfInteger) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < numberOfInteger; i++) {
-            int s = randomNumber(interval)+1;
-        if (!(list.contains(s))){list.add(s);}else{numberOfInteger++;}
-
+            int s = randomNumber(interval) + 1;
+            if (!(list.contains(s))) {
+                list.add(s);
+            } else {
+                numberOfInteger++;
+            }
         }
-       Collections.sort(list);
+        Collections.sort(list);
         return list;
     }
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-
-
-
-
-
+}
--- src/test/java/week08d02/LotteryTest.java
+++ src/test/java/week08d02/LotteryTest.java
@@ -2,19 +2,13 @@ package week08d02;
 
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LotteryTest {
+class LotteryTest {
 
 
     @Test
-    public void getnumbersTest() {
+    void getNumbersTest() {
 
         assertEquals(4, Lottery.getNumbers(90, 4).size());
 
@@ -22,7 +16,7 @@ public class LotteryTest {
 
 
     @Test
-    public void getnumbersTest2() {
+    void getNumbersTest2() {
 
         assertEquals("[1, 2, 3, 4, 5]", Lottery.getNumbers(5, 5).toString());
 
