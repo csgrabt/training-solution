b580cc7e88944f8151b79d1688ad03d454a14659
csgrabt <gy.cseko@outlook.com>
2021-04-08 15:20:17 +0200

    refactor

--- src/test/java/week08d03/junior/StringListsTest.java
+++ src/test/java/week08d03/junior/StringListsTest.java
@@ -1,4 +1,4 @@
-package week08d03;
+package week08d03.junior;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,22 +9,14 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static week08d03.StringLists.stringListsUnion;
 
-public class StringListsTest {
+class StringListsTest {
 
 
     @Test
-    public void getnumbersTest() {
-List<String> list1 = new ArrayList<>();
-list1.add("a");
-list1.add("b");
-list1.add("c");
-list1.add("d");
-List<String> list2 = new ArrayList<>();
-list2.add("a");
-list2.add("b");
-list2.add("c");
-list2.add("d");
-list2.add("e");
+    void getNumbersTest() {
+        List<String> list1 = List.of("a", "b", "c", "d");
+        List<String> list2 = List.of("a", "b", "c", "d", "e");
+
 
         assertEquals("[a, b, c, d, e]", stringListsUnion(list1, list2).toString());
 
